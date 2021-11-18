e1a6f0c9cb41f9d5c0398527c1938106b7398897
prashant09.thakur <prashant09.thakur@gmail.com>
2021-11-18 13:49:04 +0530

    modified dto

--- src/main/java/com/reactive/productservice/dto/ProductDto.java
+++ src/main/java/com/reactive/productservice/dto/ProductDto.java
@@ -1,13 +1,20 @@
 package com.reactive.productservice.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data//getter and setter
 @ToString
+@NoArgsConstructor
 public class ProductDto {
 
 	private String id;
 	private String description;
 	private Integer price;
+	
+	public ProductDto(String description,Integer price) {
+		this.description=description;
+		this.price=price;
+	}
 }
